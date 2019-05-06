122bd7b0b1d3ece47b6fcbef1a657687a29573f0
Paweł <pbokotko@gmail.com>
2019-05-06 14:37:58 +0200

    Dodaj pliki projektów.

---
+++ WykrywanieIncludow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WykrywanieIncludow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
